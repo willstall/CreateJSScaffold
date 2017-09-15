44a6b64804b35e3974c97e94a9a37dec91951ddd
Will Stallwood <willstall@users.noreply.github.com>
2017-09-15 11:42:17 -0400

    Testing

--- cs/movment/oscillate/OscillateScale.cs
+++ cs/movment/oscillate/OscillateScale.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class OscillateScale : MonoBehaviour
 {
-    
     public float frequency = 2;
     public Vector3 amplitude = Vector3.one; 
     public bool randomOffset;   
--- js/components.js
+++ js/components.js
@@ -1,3 +1,57 @@
+//OSCILLATE SCALE
+/*
+using UnityEngine;
+using System.Collections;
+
+public class OscillateScale : MonoBehaviour
+{
+    
+    public float frequency = 2;
+    public Vector3 amplitude = Vector3.one; 
+    public bool randomOffset;   
+    
+
+    float offset;
+
+    Vector3 lastOffset;
+
+    void Start()
+    {
+        offset = 0.0f;
+        if(randomOffset)
+            offset = Random.Range(0,10000);
+    }
+        
+    void Update ()
+    {
+        var thisOffset = Mathf.Sin( (Time.time + offset) * frequency ) * amplitude;
+        transform.localScale += thisOffset - lastOffset;
+        lastOffset = thisOffset;
+    }
+}
+*/
+
+function OscillateScale(){}
+var p = createjs.extend( OscillateScale, Component );
+
+p.OnAdd = function()
+{
+  this.frequency = 2;
+  this.xScaleAmplitude = 1;
+  this.yScaleAmplitude = 1;  
+  this.randomOffset = false;
+  this.offset = 0;
+  
+  if(randomOffset)
+    offset = Mathf.random() * 10000;
+}
+
+p.OnUpdate = function()
+{
+//=  var currentOffset = Mathf.sin(  (Time.time + offset) * frequency ) * amplitude;
+//  this.scaleX = 
+}
+
 // OSCILLATE COMPONENT - TEST
 function OscillateComponent(){}
 
@@ -8,7 +62,7 @@ p.OnAdd = function()
 	this.originX = this.parent.x;
 	this.counter = 0;
 	this.increment = .1;
-	this.amplitude = 50;
+	this.amplitude = 50;	
 }
 
 p.OnUpdate = function()
