fa1b03a7e77fbabc4e8ac0195e3ee26a89449bcc
Will Stallwood <willstall@users.noreply.github.com>
2017-09-14 23:25:07 -0400

    Fixed Ticker Update w/ New CreateJS

--- js/stage.js
+++ js/stage.js
@@ -10,7 +10,7 @@ function setup()
     // Update
     createjs.Ticker.timingMode = createjs.Ticker.RAF;
     createjs.Ticker.addEventListener( "tick", tick );
-    createjs.Ticker.setFPS( 30 );
+    createjs.Ticker.framerate = 30;
 
     // Container
     container = new createjs.Container();
