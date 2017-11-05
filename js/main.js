0af496c1536d2ad59012d7c574fdb32c42368b5b
Will Stallwood <willstall@users.noreply.github.com>
2017-11-05 16:11:30 -0500

    Added MouseListeners

--- js/main.js
+++ js/main.js
@@ -8,57 +8,28 @@ function main()
 	
 	// Tinder
 	var profile = new Profile();
-		
+		profile.on("pressup", OnPressUp, this );
+		profile.on("pressmove", OnPressMove, this );
+		profile.on("mousedown", OnMouseDown, this );
 		
 	container.addChild( profile );
 
 
+}
 
+function OnMouseDown( event )
+{
+ console.log("down");
+}
 
-/*
-	// Components
-//	var spinComponent = new SpinComponent();
-//		spinComponent.targetRotation = 3600;
-//		spinComponent.ease = 0.01;
+function OnPressUp( event )
+{
+ console.log("up");
+}
 
-	var positionComponent = new OscillatePositionComponent();
-		positionComponent.amplitude.y = 50;
-	var lookAtComponent = new LookAtComponent();
-	var rotateComponent = new RotateComponent( .5 );
-//		rotateComponent.increment = 0.5
-
-	// Display 
-	var test1 = new createjs.Shape();
-		test1.graphics.beginFill("DeepSkyBlue").rect(-25,-25,50,50);
-		test1.rotation = 45;
-		test1.x = 60;
-		test1.AddComponent( new OscillateScaleComponent(20, new createjs.Point(1,0) ) );
-		test1.AddComponent( new SpinComponent(0.01,3600) );
-		test1.SetComponentsUpdate( true );
-	
-	var test2 = new createjs.Shape();
-		test2.graphics.beginFill("Red").drawCircle(0, 0, 10);
-		test2.AddComponent( positionComponent );
-		test2.SetComponentsUpdate( true );
-
-	var test3 = new createjs.Shape();
-		test3.x = -60;
-		test3.graphics.beginFill("Green").rect(-30, -25, 60,50);
-		test3.AddComponent( lookAtComponent );
-		test3.SetComponentsUpdate( true );
-		
-		lookAtComponent.target = test2;
-		//test2.on("tick", update);
-
-	// Extension
-  	var extend_test = new ExtendedContainer();
-		extend_test.output();
-
-	container.addChild(test1,test2,test3);
-	container.AddComponent( rotateComponent );
-	container.SetComponentsUpdate( true );
-	
-	*/
+function OnPressMove( event )
+{
+ console.log("moving");
 }
 
 function keyPressed( event )
